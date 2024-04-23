47d17d77dc9cd0aacc0d7827b3de023587df3cd5
ghbishal <bishalk2056@gmail.com>
2024-04-23 18:22:59 +0900

    feat: education data

---
+++ src/data/education.ts
@@ -0,0 +1,9 @@
+export const EducationData = [
+  {
+    id: 1,
+    subject: 'Electronics, Communication and Information Engineering',
+    instituteName: 'Pulchowk Campus, IOE, Tribhuvan University',
+    country: 'Nepal',
+    grade: 'Bachelor Graduate',
+  },
+] as const;
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,2 +1,1 @@
 export * from './education';
-export * from './experience';
