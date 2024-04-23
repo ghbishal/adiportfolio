cea23cb711ce4e9c8de79db15a7f7107ed56e849
ghbishal <bishalk2056@gmail.com>
2024-04-23 18:44:10 +0900

    fix: site and values

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,10 @@
 export const SITE = {
   title: 'Adi',
   description: 'UI/UX Designer / Frontend Developer',
-  firstName: '',
+  firstName: 'Aditya',
+  lastName: 'Pandey',
+  aboutMe:
+    'Besides designing, I love playing games (especially VALORANT), listening to music and just chilling with my friends.',
   email: 'mraditya2017@gmail.com',
   facebookLink: 'https://www.facebook.com/ad1.tya03/',
   instagramLink: 'https://www.instagram.com/ad1.tya_pandey/',
--- src/features/home/Hero.tsx
+++ src/features/home/Hero.tsx
@@ -1,7 +1,9 @@
 import Image from 'next/image';
 import { ReactElement } from 'react';
+import { SITE } from '@/config';
 
 export const Hero = (): ReactElement => {
+  const designerDescription = SITE.description.split(' / ')[0];
   return (
     <div className="main relative z-10 flex snap-start flex-wrap justify-center pt-40 sm:flex-nowrap sm:justify-between lg:h-screen">
       <div className="absolute -top-7 flex size-[22rem] flex-col items-center justify-center sm:absolute sm:right-8 sm:top-8 lg:relative lg:inset-0">
@@ -10,9 +12,9 @@ export const Hero = (): ReactElement => {
         <hr className="undEff top-[156px] w-11/12" />
         <hr className="undEff top-[246px] w-11/12" />
         <h1 className="text-center uppercase text-purple-500 sm:text-left">
-          Aditya
+          {SITE.firstName}
           <br />
-          Pandey
+          {SITE.lastName}
         </h1>
       </div>
       <div className="z-10 flex flex-col items-center sm:relative sm:-z-10 lg:z-0 ">
@@ -32,11 +34,8 @@ export const Hero = (): ReactElement => {
         <hr className="z-20 w-4/5 border-neutral-500" />
       </div>
       <div className="mt-auto flex w-[22rem] flex-col items-center gap-[1rem] text-center sm:items-start sm:text-left lg:static">
-        <h3>UI/UX DESIGNER</h3>
-        <p className="text-neutral-400">
-          Besides designing, I love playing games (especially VALORANT), listening to music and just
-          chilling with my friends.
-        </p>
+        <h3>{designerDescription}</h3>
+        <p className="text-neutral-400">{SITE.aboutMe}</p>
       </div>
     </div>
   );
