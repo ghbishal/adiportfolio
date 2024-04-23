1fee1ee733cb00ac51d0348aa22942309730d0f8
ghbishal <bishalk2056@gmail.com>
2024-04-23 23:37:50 +0900

    feat: next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
-/** @type {import('next').NextConfig} */
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   /* config options here */
 };
+
 export default nextConfig;
