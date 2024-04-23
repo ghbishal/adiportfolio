39cf0c808f954bf10f4e7ac73d3ed81d82440112
ghbishal <bishalk2056@gmail.com>
2024-04-23 18:51:29 +0900

    fix: image width height

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,7 +6,7 @@ import { usePathname } from 'next/navigation';
 import React, { ReactElement } from 'react';
 import { MainLayout } from '../layout';
 import { cn } from '@/lib/utils';
-
+import LOGO from 'public/logo.svg';
 export function Navbar(): ReactElement {
   const currentPathName = usePathname();
 
@@ -14,7 +14,7 @@ export function Navbar(): ReactElement {
     <main className="bg-neutral-950">
       <MainLayout className="py-4 sm:py-4">
         <div className={`inset-0 z-50 mx-auto flex h-max snap-start items-center justify-between `}>
-          <Image src="/logo.svg" alt="Adi Logo" width={40} height={24} className="grayscale" />
+          <Image src={LOGO} alt="Adi Logo" width={40} height={24} className="grayscale" />
           <div className="flex gap-2">
             <Link
               className={cn('text-sm capitalize text-neutral-300 hover:blur-[1px]', {
